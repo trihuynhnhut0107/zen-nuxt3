d31f39b58051df242466ad46d9d13605e114db8a
Tri Huynh <22521519@gm.uit.edu.vn>
2024-02-15 16:39:28 +0700

    init zen-nuxt3

---
+++ pages/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>This is a paragraph</p>
+    <p>This is the second paragraph</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inline-flex">
+    <div class="mt-2 mr-2 font-bold">Click this button</div>
+    <button
+      class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+    >
+      Button
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
